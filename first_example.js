b2b788ccd1be0935d38f297c48d1d9345864c691
Amreeta Choudhury <amreeta89@yahoo.com>
2016-07-18 19:20:03 -0400

    Created two different servers

--- first_example.js
+++ first_example.js
@@ -1,14 +1,26 @@
 var http = require('http');
 
-var PORT = 7000;
+var goodNews = 7000;
 
-function handleRequest(request, response) {
-	response.end('It Works!! Path Hit: ' + request.url);
+var badNews = 7500;
+
+function handleGoodNews(request, response) {
+	response.end('Good News! Path Hit: ' + request.url);
+
+}
+function handleBadNews(request, response) {
+	response.end('Bad News! Path Hit: ' + request.url);
 
 }
 
-var server = http.createServer(handleRequest);   //connecting to a server, to a port
+var goodNewsServer = http.createServer(handleRequest);   //connecting to a server, to a port
 
-server.listen(PORT, function(){
-	console.log("Server listening on: http://localhost:%s", PORT);
+var badNewsServer = http.createServer(handleRequest);
+
+goodNewsServer.listen(goodNews, function(){
+	console.log("Server listening on: http://localhost:%s", goodNews);
+});
+
+goodNewsServer.listen(badNews, function(){
+	console.log("Server listening on: http://localhost:%s", badNews);
 });
